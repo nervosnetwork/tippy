de02a1d27593200a9e44588ca0d843e71bb5137a
James Chen <james@ashchan.com>
2020-11-07 16:56:46 +0900

    Revert "Remove Razor.RuntimeCompilation"
    This reverts commit 2a08bf7be7308e0836830500222698c122f3cc37.

--- src/Tippy/Startup.cs
+++ src/Tippy/Startup.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Tippy
 {
@@ -19,7 +25,7 @@ namespace Tippy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             services.AddSpaStaticFiles(configuration =>
             {
