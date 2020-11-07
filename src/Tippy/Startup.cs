2a08bf7be7308e0836830500222698c122f3cc37
James Chen <james@ashchan.com>
2020-11-07 16:55:05 +0900

    Remove Razor.RuntimeCompilation

--- src/Tippy/Startup.cs
+++ src/Tippy/Startup.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Tippy
 {
@@ -25,7 +19,7 @@ namespace Tippy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
 
             services.AddSpaStaticFiles(configuration =>
             {
