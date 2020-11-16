4b72f21235c1096d33264ab086bad856e4ce48c0
James Chen <james@ashchan.com>
2020-11-16 13:15:34 +0900

    Add jquery and jquery-validation to layout

---
+++ src/Tippy/Pages/Config/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tippy.Pages.Config
+{
+    public class IndexModel : PageModel
+    {
+        [Required]
+        [BindProperty]
+        public string LockArg { get; set; } = Core.Settings.GetSettings().BlockAssembler.LockArg;
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            return RedirectToPage();
+        }
+    }
+}
