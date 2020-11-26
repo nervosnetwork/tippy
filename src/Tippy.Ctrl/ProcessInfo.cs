04e55a78b59957d34c2b8baa65ee5c5920aed24d
James Chen <james@ashchan.com>
2020-11-26 22:38:04 +0900

    Simplify ProcessInfo record definition

--- src/Tippy.Ctrl/ProcessInfo.cs
+++ src/Tippy.Ctrl/ProcessInfo.cs
@@ -2,33 +2,11 @@ using Tippy.Core.Models;
 
 namespace Tippy.Ctrl
 {
-    public record ProcessInfo
+    public record ProcessInfo(int ID, Project.ChainType Chain, string NodeRpcPort,
+        string NodeNetworkPort, string IndexerRpcPort, string LockArg)
     {
-        public int ID { get; init; }
-        public Project.ChainType Chain { get; init; }
-        public string NodeRpcPort { get; init; }
-        public string NodeNetworkPort { get; init; }
-        public string IndexerRpcPort { get; init; }
-        public string LockArg { get; init; }
-
-        public ProcessInfo(int id, Project.ChainType chain, string nodeRpcPort,
-            string nodeNetworkPort, string indexerRpcPort, string lockArg)
-        {
-            ID = id;
-            Chain = chain;
-            NodeRpcPort = nodeRpcPort;
-            NodeNetworkPort = nodeNetworkPort;
-            IndexerRpcPort = indexerRpcPort;
-            LockArg = lockArg;
-        }
-
         public static ProcessInfo FromProject(Project project) =>
-            new ProcessInfo(
-                project.ID,
-                project.Chain,
-                project.NodeRpcPort,
-                project.NodeNetworkPort,
-                project.IndexerRpcPort,
-                project.LockArg);
+            new ProcessInfo(project.ID, project.Chain, project.NodeRpcPort,
+                project.NodeNetworkPort, project.IndexerRpcPort, project.LockArg);
     }
 }
