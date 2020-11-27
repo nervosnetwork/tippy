a94c7bbe338d8fc0ffa889469244fe07899ed0e9
James Chen <james@ashchan.com>
2020-11-27 11:15:25 +0900

    User can select active/current project from dashboard

---
+++ src/Tippy/Pages/Home/SwitchProject.cshtml.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tippy.Pages.Home
+{
+    public class SwitchProjectModel : PageModel
+    {
+        private readonly Tippy.Core.Data.DbContext _context;
+
+        public SwitchProjectModel(Tippy.Core.Data.DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            var projects = await _context.Projects.ToListAsync();
+            projects.ForEach(p => p.IsActive = false);
+            project.IsActive = true;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
