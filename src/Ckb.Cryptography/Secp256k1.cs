91fb5f0c64b65e8eb41c95ed0dff5d3403056a70
James Chen <james@ashchan.com>
2020-12-02 08:37:19 +0900

    Implement secp256k1 privateToPublic

---
+++ src/Ckb.Cryptography/Secp256k1.cs
@@ -0,0 +1,19 @@
+using System;
+using Cryptography.ECDSA;
+
+namespace Ckb.Cryptography
+{
+    public static class Secp256k1
+    {
+        public static byte[] PrivateKeyToPublicKey(byte[] privateKey, bool compressed)
+        {
+            return Secp256K1Manager.GetPublicKey(privateKey, compressed);
+        }
+
+        public static string PrivateKeyToPublicKey(string privateKey, bool compressed)
+        {
+            var pk = Convert.FromHexString(privateKey);
+            return Convert.ToHexString(PrivateKeyToPublicKey(pk, compressed)).ToLower();
+        }
+    }
+}
