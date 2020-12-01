4b79c3c18f602ec6503ae07fa9359de55769b21d
James Chen <james@ashchan.com>
2020-12-02 08:45:17 +0900

    Reorder Tippy.Util.Hex members

--- src/Tippy.Util/Hex.cs
+++ src/Tippy.Util/Hex.cs
@@ -6,13 +6,14 @@ namespace Tippy.Util
 {
     public class Hex
     {
+        public static UInt32 HexToUInt32(string hex) =>
+            UInt32.Parse(hex.Remove(0, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+
         public static Int64 HexToInt64(string hex) =>
             Int64.Parse(hex.Remove(0, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
 
         public static UInt64 HexToUInt64(string hex) =>
             UInt64.Parse(hex.Remove(0, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-
-        public static string UInt64ToHex(UInt64 num) => $"0x{num:x}";
 
         public static BigInteger HexToBigInteger(string hex)
         {
@@ -20,7 +21,6 @@ namespace Tippy.Util
             return BigInteger.Parse(value, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
         }
 
-        public static UInt32 HexToUInt32(string hex) =>
-            UInt32.Parse(hex.Remove(0, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+        public static string UInt64ToHex(UInt64 num) => $"0x{num:x}";
     }
 }
