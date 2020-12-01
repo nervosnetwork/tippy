aae5a18a6f640e00a5d047cbf5c082e3072d4a9a
James Chen <james@ashchan.com>
2020-12-01 17:20:39 +0900

    Create Ckb.Cryptography project and implement blake2b hashing

---
+++ src/Ckb.Cryptography/Blake2bHasher.cs
@@ -0,0 +1,22 @@
+using System;
+using Isopoh.Cryptography.Blake2b;
+using Isopoh.Cryptography.SecureArray;
+
+namespace Ckb.Cryptography
+{
+    public static class Blake2bHasher
+    {
+        static readonly byte[] personalization = new byte[] { 99, 107, 98, 45, 100, 101, 102, 97, 117, 108, 116, 45, 104, 97, 115, 104 }; // ckb-default-hash
+
+        public static byte[] ComputeHash(byte[] data)
+        {
+            Blake2BConfig config = new()
+            {
+                Personalization = personalization,
+                OutputSizeInBytes = 32
+            };
+            SecureArrayCall secureArrayCall = default!;
+            return Blake2B.ComputeHash(data, config, secureArrayCall);
+        }
+    }
+}
