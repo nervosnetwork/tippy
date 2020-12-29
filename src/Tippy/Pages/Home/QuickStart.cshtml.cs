d5432bcb5d71c94f2d3c03d696873b75ed6eaff1
James Chen <james@ashchan.com>
2020-12-29 12:04:27 +0900

    Apply style for project menu

--- src/Tippy/Pages/Home/QuickStart.cshtml.cs
+++ src/Tippy/Pages/Home/QuickStart.cshtml.cs
@@ -1,39 +1,40 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Tippy.Core.Models;
 using Tippy.Ctrl;
 
 namespace Tippy.Pages.Home
 {
-    public class QuickStartModel : PageModel
+    public class QuickStartModel : PageModelBase
     {
-        private readonly Tippy.Core.Data.DbContext _context;
-
-        public QuickStartModel(Tippy.Core.Data.DbContext context)
-        {
-            _context = context;
-        }
-
-        public void OnGet()
+        public QuickStartModel(Tippy.Core.Data.DbContext context) : base(context)
         {
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
+            var calculatingFromUsed = Projects.Count > 0;
+            var rpcPorts = Projects.Select(p => p.NodeRpcPort);
+            var networkPorts = Projects.Select(p => p.NodeNetworkPort);
+            var indexerPorts = Projects.Select(p => p.IndexerRpcPort);
+
             Project project = new()
             {
                 Name = "CKB devchain",
                 Chain = Project.ChainType.Dev,
-                NodeRpcPort = 8114,
-                NodeNetworkPort = 8115,
-                IndexerRpcPort = 8116,
-                LockArg = "0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7",
-                IsActive = true
+                NodeRpcPort = calculatingFromUsed ? rpcPorts.Max() + 3 : 8114,
+                NodeNetworkPort = calculatingFromUsed ? networkPorts.Max() + 3 : 8115,
+                IndexerRpcPort = calculatingFromUsed ? indexerPorts.Max() + 3 : 8116,
+                LockArg = "0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7"
             };
 
-            _context.Projects.Add(project);
-            await _context.SaveChangesAsync();
+            var projects = await DbContext.Projects.ToListAsync();
+            projects.ForEach(p => p.IsActive = false);
+            project.IsActive = true;
+            DbContext.Projects.Add(project);
+            await DbContext.SaveChangesAsync();
             try
             {
                 ProcessManager.Start(project);
@@ -43,7 +44,7 @@ namespace Tippy.Pages.Home
                 TempData["ErrorMessage"] = e.Message;
             }
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("/Home/Index");
         }
     }
 }
