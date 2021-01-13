4a47131433e82da06b1c146a6daf4335ff9d7159
James Chen <james@ashchan.com>
2021-01-13 18:03:10 +0900

    Fix osx/package-osx script

--- tools/osx/package-osx.sh
+++ tools/osx/package-osx.sh
@@ -4,9 +4,9 @@ ROOT_DIR=$(pwd) # Be sure to run this from root directory!
 APP_NAME="${ROOT_DIR}/Tippy.app"
 PUBLISH_OUTPUT_DIRECTORY="${ROOT_DIR}/tippy-osx-x64"
 INFO_PLIST="${ROOT_DIR}/tools/osx/Info.plist"
-ICON_FILE="${ROOT_DIR}/tools/osx/Tippy.icns""
+ICON_FILE="${ROOT_DIR}/tools/osx/Tippy.icns"
 
-dotnet publish src/Tippy/Tippy.csproj -c Release -r osx-x64 --self-containe true -o $PUBLISH_OUTPUT_DIRECTORY
+dotnet publish src/Tippy/Tippy.csproj -c Release -r osx-x64 --self-contained true -o $PUBLISH_OUTPUT_DIRECTORY
 
 if [ -d "$APP_NAME" ]
 then
@@ -20,5 +20,6 @@ mkdir "$APP_NAME/Contents/MacOS"
 mkdir "$APP_NAME/Contents/Resources"
 
 cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
-cp "$ICON_FILE" "$APP_NAME/Contents/Resources/$ICON_FILE"
-cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_NAME/Contents/MacOS"
+cp "$ICON_FILE" "$APP_NAME/Contents/Resources/Tippy.icns"
+cp -a "$PUBLISH_OUTPUT_DIRECTORY/" "$APP_NAME/Contents/MacOS"
+
