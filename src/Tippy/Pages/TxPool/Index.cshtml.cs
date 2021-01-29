8d38e80d120c75d83d042b045e616961e6ff9598
James Chen <james@ashchan.com>
2021-01-29 14:17:10 +0900

    Add empty tx pool page

---
+++ src/Tippy/Pages/TxPool/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Tippy.Pages.TxPool
+{
+    public class IndexModel : PageModelBase
+    {
+        public IndexModel(Tippy.Core.Data.DbContext context) : base(context)
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
