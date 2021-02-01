c87356bb0817f888f9098540a3821d9d3ee47c00
James Chen <james@ashchan.com>
2021-02-01 09:02:41 +0900

    Add clear tx pool feature

---
+++ src/Tippy/Pages/TxPool/Clear.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tippy.Pages.TxPool
+{
+    public class ClearModel : PageModelBase
+    {
+        public ClearModel(Tippy.Core.Data.DbContext context) : base(context)
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ActiveProject != null)
+            {
+                Rpc().ClearTxPool();
+            }
+
+            var referer = Request.GetTypedHeaders().Referer.ToString().ToLower();
+            return Redirect(referer);
+        }
+    }
+}
+
