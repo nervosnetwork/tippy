58001a9d6626c09c53542da8c91ce68e7c92414a
James Chen <james@ashchan.com>
2021-02-02 09:28:49 +0900

    Pending tx could have empty timestamp

--- src/Tippy/Helpers/DateHelper.cs
+++ src/Tippy/Helpers/DateHelper.cs
@@ -5,8 +5,15 @@ namespace Tippy.Helpers
     {
         public static DateTime TimestampToDate(string timestamp)
         { 
-            DateTime date = new (1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            return date.AddSeconds(double.Parse(timestamp) / 1000).ToLocalTime();
+            if (String.IsNullOrEmpty(timestamp))
+            {
+                return DateTime.UnixEpoch;
+            }
+            else
+            {
+                DateTime date = new (1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+                return date.AddSeconds(double.Parse(timestamp) / 1000).ToLocalTime();
+            }
         }
 
         public static DateTime HexTimestampToDate(string hex)
