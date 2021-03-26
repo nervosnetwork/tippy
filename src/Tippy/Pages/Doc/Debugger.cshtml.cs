078a1ee0083d0eb9771645417ce611b204b4f50f
James Chen <james@ashchan.com>
2021-03-26 16:45:59 +0900

    Add doc/debugger page

---
+++ src/Tippy/Pages/Doc/Debugger.cshtml.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Tippy.Pages.Doc
+{
+    public class DebuggerModel : PageModelBase
+    {
+        public DebuggerModel(Tippy.Core.Data.TippyDbContext context) : base(context)
+        {
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
+
