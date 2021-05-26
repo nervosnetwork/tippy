517ca217c77e4c612212268f29c35406c5e6b474
James Chen <james@ashchan.com>
2021-05-27 08:37:32 +0900

    Add migration to rename FailedTransactions table to RecordedTransactions
    Note: this would drop the previous FailedTransactions.

--- src/Tippy.Core/Migrations/20210526233521_RenameFailedTransactionsToRecordedTransactions.Designer.cs
+++ src/Tippy.Core/Migrations/20210526233521_RenameFailedTransactionsToRecordedTransactions.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tippy.Core.Data;
 
 namespace Tippy.Core.Migrations
 {
     [DbContext(typeof(TippyDbContext))]
-    partial class TippyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210526233521_RenameFailedTransactionsToRecordedTransactions")]
+    partial class RenameFailedTransactionsToRecordedTransactions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
