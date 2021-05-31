7261abf9ea0cf187a5120a83b91e4ebe85f356d7
James Chen <james@ashchan.com>
2021-05-31 18:23:31 +0900

    Remove comment making no sense

--- src/Tippy.Ctrl/Process/Debugger/ProcessManager.cs
+++ src/Tippy.Ctrl/Process/Debugger/ProcessManager.cs
@@ -10,7 +10,6 @@ namespace Tippy.Ctrl.Process.Debugger
         public static void Start(Project project, string scriptGroupType, string scriptHash, string txFilePath, string debugFilePath, string ioType, int ioIndex, string? binaryForDebugger)
         {
             Stop();
-            // TODO: Maybe not need project.
             ProcessInfo processInfo = ProcessInfo.FromProject(project);
             GdbProcessInstance = new GdbProcess(processInfo, debugFilePath);
             DebuggerProcessInstance = new DebuggerProcess(processInfo, scriptGroupType, scriptHash, txFilePath, ioType, ioIndex, binaryForDebugger);
